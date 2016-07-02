e8902da53863b0cc2ebd7aff8c660b324157b746
R F I D E <RFIDE@users.noreply.github.com>
2016-07-02 23:38:11 +0530

    Initial commit💯
    --/The Heart of Build System/-- of "RFIDE®".
    Gruntfile.js, configuring the JavaScript Task Runner.
    Help: Help: https://github.com/gruntjs or http://gruntjs.com/ and http://gruntjs.com/configuring-tasks
    Usage: $ grunt build

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+
+/*!
+ * RFIDE®
+ * @version   : [1.0.0]
+ * __________________________________________________________________________________
+ * R F I D E®
+ * ~~~~~~~~~~
+ * Rosalind Elsie Franklin Institute for Disease Eradication.
+ * __________________________________________________________________________________
+ * Grunt, http://gruntjs.com/.
+ * The JavaScript Task Runner.
+ * __________________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * __________________________________________________________________________________
+ * @date      : 16-June-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * __________________________________________________________________________________
+ *      Licensed under the Apache License, Version 2.0, (the "License");
+ *      you may not use this file except in compliance with the License.
+ *                  You may obtain a copy of the License at:
+ *                 http://www.apache.org/licenses/LICENSE-2.0
+ *                                     or
+ *              https://github.com/RFIDE/RFIDE/blob/master/LICENSE
+ *    Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS-IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *                        limitations under the License.
+ * __________________________________________________________________________________
+ *
+ *                  --/The Heart of Build System/-- of "RFIDE®".
+ * __________________________________________________________________________________
+ */
